51e94dd34228051ad598d384591421c2c8d9c1d0
Todd Wells <todd.wells@stelligent.com>
2019-06-06 10:43:21 -0700

    dump  event

--- index.py
+++ index.py
@@ -5,8 +5,13 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'event': json.dumps(event)
     }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -11,12 +11,6 @@ class TestHandlerCase(unittest.TestCase):
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
         self.assertIn('Hello World', result['body'])
 
-    def test_invalid_index(self):
-        print("testing response.")
-        result = index.handler(None, None)
-        print(result)
-        self.assertEqual(result['statusCode'], 404)
-
 
 if __name__ == '__main__':
     unittest.main()
